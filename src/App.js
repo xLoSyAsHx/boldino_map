8d2add355fd92ed6448846345d1e67c7402b8b08
Ivan <ivan.s.losev@mail.ru>
2019-02-08 22:04:34 +0300

    Protype 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import boldinoMap from "../src/map.svg"
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper" >
+        <CssBaseline />
+          <Grid container alignItems='stretch'>
+            <img src={boldinoMap} alt="image"/>
+            <svg viewBox="0 0 1145.28 1609.92">
+            <a href="#">
+              <path className="boldino" d="m 202.18925,205.98623 13.28944,499.30303 473.6734,-46.51302 -30.37585,-470.82567 z">
+              </path>
+            </a>
+              
+            </svg>
+
+            
+          </Grid>
       </div>
     );
   }
