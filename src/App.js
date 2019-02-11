16cc6bdfefd851ac6c4ab081bf52d19cae8bc6fc
Ivan <ivan.s.losev@mail.ru>
2019-02-11 22:41:16 +0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import boldinoMap from "../src/map2.svg"
+import boldinoMap from "../src/map.svg"
+import boldinoImg from "../src/map.jpg"
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Draggable, {DraggableCore} from 'react-draggable'; // Both at the same time
 
 
+/*
 class App extends Component {
 
+  constructor(props) {
+      super(props);
+      this.state = {
+          width:  1080,
+          height: props.height,
+      }
+
+      this.state = {height: props.height};
+  }
+
+  
+
+  componentWillMount(){
+    this.setState({height: window.innerHeight + 'px'});
+    console.log(window.innerHeight);
+  }
+
+
+
   render() {
+    console.log(window.innerWidth);
     return (
       <div className="wrapper" >
         <CssBaseline />
             
             <Draggable 
               //defaultPosition={{x: -2290, y: -3218}}
-              grid={[5, 5]}
-              //bounds={{top: -6000, left: -4000, right: 6000, bottom: 4000}}
+              //grid={[5, 5]}
+              bounds={{top: -window.innerHeight / 2, left: -0, right: 1000, bottom: window.innerHeight / 2}}
+              // viewBox="0 0 1145 1609"
               scale={1}>
               <div >
-                <svg viewBox="0 0 4000 6000">
+                
+                <svg width={1000} height={window.innerHeight} >
                 <a href="#">
-                  <path className="boldino" d="m 202.18925,205.98623 13.28944,499.30303 473.6734,-46.51302 -30.37585,-470.82567 z">
+                  <path className="boldino" d="m 0,0 0,1000 500,0 0,-1000 z">
                   </path>
                 </a>
                   
                 </svg>
-                <img src={boldinoMap} alt="image"/>
+                <img width={1000} height={window.innerHeight} src={boldinoImg} alt="image"/>
               </div>
             </Draggable>
             
@@ -38,5 +63,55 @@ class App extends Component {
     );
   }
 }
+*/
+export default class CreateContact extends Component {
+  state = {
+    windowHeight: undefined,
+    windowWidth: undefined
+  }
 
-export default App;
+  handleResize = () => this.setState({
+    windowHeight: window.innerHeight,
+    windowWidth: window.innerWidth
+  });
+
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  render() {
+    return (
+      <div className="wrapper" >
+        <CssBaseline />
+            
+            <Draggable 
+              //defaultPosition={{x: -2290, y: -3218}}
+              //grid={[5, 5]}
+              bounds={{top: -6436, left: -4580 + this.state.windowWidth, right:  0, bottom: 0}}
+               //viewBox="0 0 4580 6436"
+              scale={1}>
+              <div >
+                
+                <svg width={4580} height={6436} >
+                <a href="#">
+                  <path className="boldino" d="m 0,0 0,1000 500,0 0,-1000 z">
+                  </path>
+                </a>
+                  
+                </svg>
+                <img width={4580} height={6436} src={boldinoImg} alt="image"/>
+              </div>
+            </Draggable>
+            
+            
+            
+
+      </div>
+    );
+  }
+}
